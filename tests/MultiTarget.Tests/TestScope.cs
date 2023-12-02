9696220a37c44fb9a00c8e7aafb7d9e7f6178b96
Christopher Kimbell <christopher.kimbell@outlook.com>
2023-12-02 11:15:07 +0100

    basics

---
+++ tests/MultiTarget.Tests/TestScope.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace MultiTarget.Tests
+{
+    internal class TestScope : WebApplicationFactory<Program>
+    {
+    }
+}
---
+++ tests/MultiTarget.Tests/WeatherTests.cs
@@ -0,0 +1,16 @@
+﻿namespace MultiTarget.Tests
+{
+    public class WeatherTests
+    {
+        [Fact]
+        public async Task Run()
+        {
+            using var ts = new TestScope();
+
+            var httpClient = ts.CreateClient();
+            var result = await httpClient.GetStringAsync("http://localhost/weatherforecast/");
+
+            Assert.NotNull(result);
+        }
+    }
+}
